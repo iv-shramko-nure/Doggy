dbe1d470f682461fabec25c34aac54a8d362e7b5
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2023-05-25 19:19:57 +0300

    created shelter register components

--- frontend/DoggyWeb/src/app/auth/auth-routing.module.ts
+++ frontend/DoggyWeb/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,10 @@
+import { LegalInfoComponent } from './components/shelter-register/legal-info/legal-info.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/app/auth/components/login/login.component';
 import { RegisterComponent } from 'src/app/auth/components/register/register.component';
+import { DescriptionComponent } from 'src/app/auth/components/shelter-register/description/description.component';
+import { MainInfoComponent } from 'src/app/auth/components/shelter-register/main-info/main-info.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,18 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'shelter-register',
+    component: MainInfoComponent
+  },
+  {
+    path: 'shelter-register/description',
+    component: DescriptionComponent
+  },
+  {
+    path: 'shelter-register/legal',
+    component: LegalInfoComponent
   },
   {
     path: '**',
--- frontend/DoggyWeb/src/app/auth/auth.module.ts
+++ frontend/DoggyWeb/src/app/auth/auth.module.ts
@@ -5,12 +5,18 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from '@angular/forms';
+import { MainInfoComponent } from './components/shelter-register/main-info/main-info.component';
+import { DescriptionComponent } from './components/shelter-register/description/description.component';
+import { LegalInfoComponent } from './components/shelter-register/legal-info/legal-info.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MainInfoComponent,
+    DescriptionComponent,
+    LegalInfoComponent
   ],
   imports: [
     CommonModule,
