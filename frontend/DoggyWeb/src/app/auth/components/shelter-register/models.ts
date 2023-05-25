e484d6cb8dff97ab9f35842c448d983486a5ce79
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2023-05-25 19:40:42 +0300

    model and small fixes

---
+++ frontend/DoggyWeb/src/app/auth/components/shelter-register/models.ts
@@ -0,0 +1,10 @@
+export interface ShelterRegisterData {
+  email: string,
+  phoneNumber: string,
+  website: string,
+  name: string,
+  address: string,
+  description: string,
+  accountNumber: string,
+  payPalEmail: string,
+}
