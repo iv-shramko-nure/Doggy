ecb8683a5fb0ad417cf9b841d705b8088d965f1b
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2023-05-25 21:18:34 +0300

    craeted components

--- frontend/DoggyWeb/src/app/core/core.module.ts
+++ frontend/DoggyWeb/src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    NavBarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
