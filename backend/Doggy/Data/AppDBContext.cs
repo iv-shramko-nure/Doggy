93b75721ad6e06534c5f2be108fb39f3515b87cf
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2023-05-29 16:12:49 +0300

    db sert up

--- backend/Doggy/Data/AppDBContext.cs
+++ backend/Doggy/Data/AppDBContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
-    public class AppDBContext : DbContext
+    public class AppDBContext : IdentityDbContext
     {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+           : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
