d91e8d0fe1af41ed57fce8b5b18f52e93742537a
LEOBIT-ISHRAMKO\Ivan <ishramko@leobit.com>
2023-06-01 22:16:07 +0300

    auth test

--- frontend/DoggyWeb/src/app/core/components/user-profile/user-profile.component.ts
+++ frontend/DoggyWeb/src/app/core/components/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    console.log('logout')
+    this.router.navigate(['auth']);
   }
 
 }
